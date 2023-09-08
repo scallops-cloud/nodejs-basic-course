49d5999498fe96e2c404824f9525c882a0b06922
Turbo Panumarch <panumarch.a@gmail.com>
2023-09-08 16:26:48 +0700

    add questino to 02

--- 02-read-args/main.js
+++ 02-read-args/main.js
@@ -4,3 +4,7 @@ const filePath = "data.txt"
 const input = process.argv[2]
 
 fs.appendFileSync(filePath, input + "\n")
+
+
+// Question: How to append the text with space?
+// Sol: use ".." for input
