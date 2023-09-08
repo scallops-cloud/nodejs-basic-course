f22600c1a9bbf913f5aa3aa0930ef3c5e543153c
Turbo Panumarch <panumarch.a@gmail.com>
2023-09-08 16:38:18 +0700

    03 add write option

---
+++ 03-args-option/main.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+const filePath = "data.txt";
+
+const option = process.argv[2]
+const input = process.argv[3]
+
+
+if (option === 'w') {
+  fs.appendFileSync(filePath, input)
+  console.log('appended:', input)
+}
+else {
+  console.log('invalid option')
+}
+
+// Challenge: 
+// 1. Add "read" option
+// 2. Add "clear" option
+
