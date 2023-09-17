2bda57be8f49f3b2dbd431034d8716850becc6e7
Turbo Panumarch <panumarch.a@gmail.com>
2023-09-17 15:11:52 +0700

    update: exercises for express hello world

--- day-1/solutions/09-express-hello-world/app.js
+++ day-1/solutions/09-express-hello-world/app.js
@@ -7,10 +7,12 @@
  * 1. config express to expose port 8000
  * 2. calling to "http://localhost:8000" return "Hello my friends"
  * 3. calling to "http://localhost:8000/about-us" return "About Us"
+ * 4. Use "npm start" to start the application
  *
  * ## Challenges:
- * 1. return the 401 error with the error message when accessing "http://localhost:8000/admin".
- * 2. redirect the "http://localhost:8000/home" to "http://localhost:8000/about-us"
+ * 1. Install "nodemon" to auto-reload the code with "npm start"
+ * 2. return the 401 error with the error message when accessing "http://localhost:8000/admin".
+ * 3. redirect the "http://localhost:8000/home" to "http://localhost:8000/about-us"
  *
  * ## Hint:
  * - https://expressjs.com/en/starter/installing.html
