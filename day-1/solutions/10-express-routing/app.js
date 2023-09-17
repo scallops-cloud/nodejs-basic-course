3cd399ecc9d9de50c2708c78340ca578a656e833
Turbo Panumarch <panumarch.a@gmail.com>
2023-09-17 17:26:31 +0700

    solution: 10 execise

--- day-1/solutions/10-express-routing/app.js
+++ day-1/solutions/10-express-routing/app.js
@@ -31,8 +31,35 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 app.get("/", (req, res) => {
   res.send("Hello my friends");
+});
+
+app.get("/users", (req, res) => {
+  const userId = req.query.id;
+  res.send(`User ID: ${userId}`);
+});
+
+app.post("/users", (req, res) => {
+  const userId = req.query.id;
+  const name = req.body.name;
+
+  res.send(`User ID: ${userId}, name has been updated to ${name}`);
+});
+
+app.get("/users/:userId", (req, res) => {
+  const userId = req.params.userId;
+  res.send(`User ID: ${userId}`);
+});
+
+app.post("/users/:userId", (req, res) => {
+  const userId = req.params.userId;
+  const name = req.body.name;
+
+  res.send(`User ID: ${userId}, name has been updated to ${name}`);
 });
 
 app.listen(port, () => {
