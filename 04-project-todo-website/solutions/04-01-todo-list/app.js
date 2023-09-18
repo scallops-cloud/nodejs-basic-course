c4a069afd0821513febe8dab4286027e57150957
Turbo Panumarch <panumarch.a@gmail.com>
2023-09-18 15:50:13 +0700

    add: 04-01 todo website exercise

---
+++ 04-project-todo-website/solutions/04-01-todo-list/app.js
@@ -0,0 +1,28 @@
+/**
+ * # Express.js Todo Website - Todo List
+ *
+ * Create a Todo website using an existing Todo CLI to convert to web version.
+ *
+ * ## Specs:
+ * 1. Todo List: "http://localhost:8000/todos"
+ *    - Return the website with title "Todo App"
+ *    - Display the list of todos we have with the number in front starting from 1.
+ *
+ * 2. Done button:
+ *    - For each todo item, there is a button "Done".
+ *    - When click "Done", the website submits request to "http://localhost:8000/todos/NUMBER".
+ *    - After finish updating
+ *
+ * ## Challenges:
+ * 1. Question: What do you learn from this project?
+ *
+ * 2. New button at the bottom, "Show done items".
+ *  - When click, it navigates to "http://localhost:8000/todos?show_done=1".
+ *  - The button becomes "Hide done items".
+ *  - Display the list of done items under the list
+ *
+ * 3. Implement "Hide done items" in your understanding.
+ *
+ * 4. Make the todo more beautiful with CSS and images.
+ *
+ */
