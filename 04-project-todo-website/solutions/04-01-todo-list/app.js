193bc097b8e23c59251159dc209652a5782dd360
Turbo Panumarch <panumarch.a@gmail.com>
2023-09-18 16:26:35 +0700

    update: 04-01 solution

--- 04-project-todo-website/solutions/04-01-todo-list/app.js
+++ 04-project-todo-website/solutions/04-01-todo-list/app.js
@@ -7,11 +7,12 @@
  * 1. Todo List: "http://localhost:8000/todos"
  *    - Return the website with title "Todo App"
  *    - Display the list of todos we have with the number in front starting from 1.
+ *    - Try adding todos to data.txt manually to see the result changed.
  *
  * 2. Done button:
  *    - For each todo item, there is a button "Done".
- *    - When click "Done", the website submits request to "http://localhost:8000/todos/NUMBER".
- *    - After finish updating
+ *    - When click "Done", the website submits request to "http://localhost:8000/todos/NUMBER/done".
+ *    - After finish updating, redirect to Todo List page.
  *
  * ## Challenges:
  * 1. Question: What do you learn from this project?
@@ -26,3 +27,29 @@
  * 4. Make the todo more beautiful with CSS and images.
  *
  */
+
+const express = require("express");
+const { read, markDone } = require("./utils");
+
+const app = express();
+const port = 8000;
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static("public"));
+
+app.get("/todos", (req, res) => {
+  const todos = read();
+
+  res.render("todo-list.ejs", { todos });
+});
+
+app.post("/todos/:todoIndex/done", (req, res) => {
+  markDone(req.params.todoIndex);
+
+  res.redirect("/todos");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ 04-project-todo-website/solutions/04-01-todo-list/utils.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+
+const filePath = "data.txt";
+const doneFilePath = "done.txt";
+
+function write(input) {
+  fs.appendFileSync(filePath, input + "\n");
+
+  printHeader();
+  console.log("appended:", input);
+}
+
+function read() {
+  const content = fs.readFileSync(filePath);
+  const text = content.toString();
+  const list = text.trim().split("\n");
+
+  return list;
+}
+
+function clear() {
+  printHeader();
+  console.log("the file has been clear");
+}
+
+function printHeader() {
+  console.log("Todo App!" + "\n");
+}
+
+function markDone(itemNumber) {
+  // 1. read all
+  const content = fs.readFileSync(filePath);
+  const text = content.toString();
+  const list = text.trim().split("\n");
+
+  if (itemNumber < 0 || itemNumber > list.length) {
+    console.log("invalid item number");
+    return;
+  }
+
+  // 2. find the line from itemNumber
+  // 3. remove that line
+  const doneItem = list.splice(itemNumber, 1);
+  console.log("found ", doneItem);
+
+  // 4. append removed item to the done.txt
+  fs.appendFileSync(doneFilePath, doneItem.join("\n") + "\n");
+
+  // 5. write the new set of todos without removed line
+  fs.writeFileSync(filePath, list.join("\n") + "\n");
+
+  console.log(`marked ${itemNumber}. as done.`);
+}
+
+module.exports = {
+  write,
+  read,
+  clear,
+  markDone,
+};
