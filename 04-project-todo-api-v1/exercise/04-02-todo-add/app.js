603752f1590eb3c499d40f2bd1bb6dc77a0062d7
Turbo Panumarch <panumarch.a@gmail.com>
2023-09-25 21:43:23 +0700

    update: 04-02 exercise to be API

---
+++ 04-project-todo-api-v1/exercise/04-02-todo-add/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { findTodo, listTodos } from "./models/todo.js";
+
+const app = express();
+const port = 8000;
+
+app.get("/todos", (req, res) => {
+  const todos = listTodos();
+  res.json({ data: todos });
+});
+
+app.get("/todos/:todoId", (req, res) => {
+  const todoId = parseInt(req.params.todoId, 10);
+  const todo = findTodo(todoId);
+
+  if (!todo) {
+    res.status(404).json({ error: { message: "todo not found" } });
+    return;
+  }
+
+  res.json({ data: todo });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
