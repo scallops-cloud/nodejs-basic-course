68307fdd4046c4b50b1a0e31965dfe68859a55c5
Wiwatta Mongkhonchit <zentetsukenz@gmail.com>
2024-01-14 21:19:04 +0700

    Ex1 shape up

--- 05-crud-rest-api/exercises/05-01-todo-postman-collection/models/todo.js
+++ 05-crud-rest-api/exercises/05-01-todo-postman-collection/models/todo.js
@@ -7,23 +7,20 @@ const todoDatabase = [
 ];
 
 export const listTodos = () => {
-  // Implement logic here to list all todos
   return todoDatabase;
 };
 
 export const findTodo = (todoId) => {
-  // Implement logic here to find a single todo by id
   return todoDatabase.find((todo) => todo.id === todoId);
 };
 
 export const createTodo = ({ title }) => {
-  // Implement logic here to create a new todo
-  // 1. Create todo object with the new id from `nextId`
-  const newTodo = { id: nextId, title };
-  // 2. Push the new todo object
-  todoDatabase.push(newTodo);
-  // 3. increment the `nextId`
-  nextId++;
-  // 4. return a new todo
-  return newTodo;
+  const todo = {
+    id: nextId++,
+    title,
+  };
+
+  todoDatabase.push(todo);
+
+  return todo;
 };
